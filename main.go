d891c7ea712400761b80b8e4de48aec3a379c1fa
Mirco Liese <mliese@users.noreply.github.com>
2018-06-11 17:22:08 +0200

    add travis.yml for ci with sample test

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 )
 
+func Sum(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
