0508f60842685183659b60df7b8bd573be9d95ea
Mirco Liese <mliese@users.noreply.github.com>
2018-06-11 17:34:31 +0200

    change testSum test value from 10 to 6

--- main_test.go
+++ main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSum(t *testing.T) {
 	total := Sum(1, 5)
 	if total != 6 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 6)
 	}
 }
