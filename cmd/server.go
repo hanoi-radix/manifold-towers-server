46903d86fc8142ba9b859f40a00de2a7b8362c9a
Friedemann Sommer <friedemannsommer@yahoo.de>
2018-06-14 01:20:32 +0200

    updated go import path

--- cmd/server.go
+++ cmd/server.go
@@ -1,8 +1,7 @@
 package main
 
-// this isn't working expected... most likely a issue with the current $GOPATH
-// import "github.com/hanoi-radix/manifold-towers-server/internal/server"
+import "github.com/hanoi-radix/manifold-towers-server/internal/server"
 
 func main() {
-	// manifoldserver.Start()
+	manifoldserver.Start()
 }
