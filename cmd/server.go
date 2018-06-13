5ad7a5b115a8bcc3a51e7ec4fbe80a77adc61200
Friedemann Sommer <friedemannsommer@yahoo.de>
2018-06-14 01:38:06 +0200

    rename "server" -> "manifoldserver"

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/hanoi-radix/manifold-towers-server/internal/server"
+import "github.com/hanoi-radix/manifold-towers-server/internal/manifoldserver"
 
 func main() {
 	manifoldserver.Start()
